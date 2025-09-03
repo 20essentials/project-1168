69f17fdb6b307affd3f3b516645e3e6bf89770f3
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 12:39:22 +0200

    v0.0.2

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,5 +2,11 @@ export const $ = (el: string) => document.querySelector(el);
 export const $$ = (el: string) => document.querySelectorAll(el);
 
 export function baseUrl(path: string) {
-  return new URL(path.replace(/^\/+/, ""), import.meta.env.SITE).toString();
+  return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
+}
+
+export function getUrlOfTheComponent({ theURL }: { theURL: string }) {
+  const url = new URL(theURL);
+  const fileName = url.pathname.split('/').pop();
+  return `https://github.com/20essentials/project-1168/blob/main/src/components/${fileName}`;
 }
