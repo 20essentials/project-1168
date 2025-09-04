46e432661189baddbca3ebd7d3cb8e238f124469
DominicDev <s0l0estudi4r@gmail.com>
2025-09-04 15:52:11 +0200

    v0.2.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   build: {
     assets: 'static'
   },
+
   //Deploy normal project
-  base: '/project-1168/',
-  site: 'https://20essentials.github.io/project-1168/'
+  // base: '/project-1168/',
+  // site: 'https://20essentials.github.io/project-1168/'
 
   //Deploy 20 Projects
   // base: '/20-projects-with-css/',
   // site: 'https://20essentials.github.io/20-projects-with-css/'
 
   //Localhost
-  // site: 'http://localhost:4321/'
+  site: 'http://localhost:4321/',
+
+  adapter: vercel()
 });
